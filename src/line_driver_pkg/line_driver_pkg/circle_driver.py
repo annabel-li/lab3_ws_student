c2a5499f323c96d9c7d1e9e842620604e581649a
fizzer <li.annabel47@gmail.com>
2026-09-23 18:55:17 -0700

    the robot line follows

--- src/line_driver_pkg/line_driver_pkg/circle_driver.py
+++ src/line_driver_pkg/line_driver_pkg/circle_driver.py
@@ -1,21 +1,32 @@
+
 #!/usr/bin/env python3
 
 import rclpy
-import time
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+import time
 
 class Circle_Driver_Node(Node):
-   def __init__(self):
-       super().__init__(node_name='driver_node')
-       # TODO Add code to drive in circles here
-       # END
+    def __init__(self):
+        super().__init__(node_name='driver_node')
+        self.publisher_ = self.create_publisher(msg_type=Twist, topic='/cmd_vel', qos_profile=1)
+        
+        time.sleep(1.0)
+        self.move = Twist()
+        self.move.linear.x = 0.0
+        self.move.angular.z = 0.3
+        self.publisher_.publish(msg=self.move)
+        print("Done running constructor.")
+        self.publisher_.publish(msg=self.move)
+
 
 def main(args=None):
+    print("Starting...")
     rclpy.init(args=args)
     cdn = Circle_Driver_Node()
     rclpy.spin(cdn)
     rclpy.shutdown()
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     main()
--- src/line_driver_pkg/line_driver_pkg/line_driver.py
+++ src/line_driver_pkg/line_driver_pkg/line_driver.py
@@ -1,19 +1,76 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-import cv2
+import cv2 as cv
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np 
+
+LINEAR_SPEED = 0.2
+ANGULAR_GAIN = 0.01 
+
+def longestConnectedColour(imgRow, targetColour):
+    """!
+    @brief Finds the longest connected string of pixels in an image's row with pixel value equal to targetColour.
+
+    @param imgRow An array representing the pixel values in the row of an image. Must be grayscale.
+    @param targetColour An int representing the target colour.
+
+    @return An array storing the index numbers of the longest connected strip of targetColour in that row.
+    """
+    maxLen = 0
+    currentIndices = []
+    longestString = []
+
+    # if the current pixel is the target colour, add it to the list of indices
+    for i in range(len(imgRow)):
+        if imgRow[i] == targetColour:
+            currentIndices.append(i)
+        # else, we have either reached the end of the strip and should check for the strip's length
+        else:
+            if len(currentIndices) > maxLen:
+                maxLen = len(currentIndices)
+                longestString = currentIndices
+            currentIndices = []  # reset regardless of whether it was the longest
+
+    # save the list if it was the longest
+    if len(currentIndices) > maxLen:
+        longestString = currentIndices
+
+    return longestString
+
+
+def findLineCenter(processedFrame, targetColour): 
+
+    # Get the image dimensions
+    height, width = processedFrame.shape
+
+    # start from bottom row, go upward
+    for row in range(height - 1, -1, -1):
+        # only worth checking rows that actually contain the colour
+        if np.any(processedFrame[row] == targetColour):
+            road = longestConnectedColour(processedFrame[row], targetColour)
+
+            # get the x/y coordinates for the center of the circular marker
+            cx = road[len(road) // 2]
+            cy = row
+
+            return cx, cy 
+
+    return None, None 
 
 class LineFollower(Node):
+
     def __init__(self):
+
         super().__init__('line_follower')
-        self.bridge = CvBridge()
-        self.image_sub = self.create_subscription(Image, 'image_topic',
+        self.bridge = CvBridge() #CvBridge handles the conversion of raw ROS2 data --> NumPy array 
+        self.image_sub = self.create_subscription(Image, '/camera/image_raw',
                                                   self.callback, 1)
 
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 1)
+        self.trajectories = [] 
 
     def callback(self, data):
 
@@ -23,9 +80,35 @@ class LineFollower(Node):
             self.get_logger().error(str(e))
             return
 
-        #TODO fill in the details for processing an image to determine the position
-        # of the line in the image and publishing a command to move the robot forward
-        # while the line is in the center.
+        height, width, _ = cv_image.shape 
+        # Convert BGR to grayscale. 'grayFrame' is a new copy of the original frame, so the information about the original colours are preserved in 'frame'
+        grayFrame = cv.cvtColor(cv_image, cv.COLOR_BGR2GRAY)
+        # Threshold the image to make the road 'pop' --> use THRESH_BINARY_INV so pixels ABOVE threshold become black (0)
+        _, thresh = cv.threshold(grayFrame, 60, 255, cv.THRESH_BINARY_INV)
+        lineX, lineY = findLineCenter(thresh, 255) #find the center of the line 
+
+        move = Twist() #creates a new empty "Twist" message object (https://ros2course.readthedocs.io/en/latest/Topics.html) 
+
+        if lineX is not None: 
+
+            imgCenter = width/2 
+            diff = imgCenter - lineX
+            self.trajectories.append(diff)
+            move.linear.x = LINEAR_SPEED
+            move.angular.z = ANGULAR_GAIN * diff 
+
+        elif len(self.trajectories) > 0: 
+
+            move.linear.x = LINEAR_SPEED 
+            move.angular.z = ANGULAR_GAIN * self.trajectories[-1]
+
+        else: 
+
+            move.linear.x = 0 
+            move.angular.z = 0 
+
+        self.cmd_vel_pub.publish(move)
+
 
 
 def main(args=None):
@@ -37,7 +120,7 @@ def main(args=None):
     except KeyboardInterrupt:
         print("Shutting down")
     finally:
-        cv2.destroyAllWindows()
+        cv.destroyAllWindows()
         lf.destroy_node()
         rclpy.shutdown()
 
